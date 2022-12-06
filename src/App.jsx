500be8de4fea4ef4edf052ebae13fe5db2808499
piero965d <piero965d@gmail.com>
2022-12-05 19:25:14 -0500

    Añadiendo Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styled from '@emotion/styled'
 import ImagenCripto from './img/imagen-criptos.png'
+import Formulario from './components/Formulario'
+import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 
 const Contenedor = styled.div`
   max-width: 900px;
   margin: 0 auto;
   width: 90%;
-  @media (min-width: 992px){
+  @media (min-width: 992px) {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
     column-gap: 2rem;
@@ -27,23 +30,48 @@ const Heading = styled.h1`
   margin-top: 80px;
   margin-bottom: 50px;
   font-size: 34px;
-  &::after{
+  &::after {
     content: '';
     width: 100px;
     height: 6px;
     background-color: #66A2FE;
     display: block;
-    margin: 10px auto 0 auto
+    margin: 10px auto 0 auto;
   }
 `
 
 function App() {
+
+  const [monedas, setMonedas] = useState({})
+  const [resultado, setResultado] = useState([])
+  const [cargando, setCargando] = useState(false)
+
+  useEffect(() => {
+    if(Object.keys(monedas).length > 0){
+      const cotizarCripto = async () => {
+        setCargando(true)
+        setResultado({})
+        const {moneda, criptomoneda} = monedas
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+        setResultado(resultado.DISPLAY[criptomoneda][moneda]);
+        setCargando(false)
+      }
+      cotizarCripto()
+    }
+  }, [monedas])
 
   return (
     <Contenedor>
       <Imagen src={ImagenCripto} alt='Imagen Cryto'/>
       <div>
         <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario
+          setMonedas={setMonedas}
+        />
+      {cargando && <Spinner/>}
+      {resultado.PRICE &&<Resultado resultado={resultado}/>}
       </div>
     </Contenedor>
   )
